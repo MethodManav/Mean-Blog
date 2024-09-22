80bce60a446e2943bd52ade28017c9b254c3d2ef
Behera Manav <manavbehera123@gmail.com>
2024-09-22 12:19:25 +0530

    crated auth middleware

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,12 +1,21 @@
-import { Context } from "hono";
+import { Context, Next } from "hono";
+import { verify } from "hono/jwt";
 
 class AuthMiddleware {
-  async authUser(c: Context) {
-    // const userToken=c.header.
-
-    return c.json({
-      message: "jii",
-    });
+  async authUser(c: Context, next: Next) {
+    const authHeader = c.req.header("authorization");
+    const user = await verify(authHeader || "", c.env.JWT);
+    if (user) {
+      c.set("userId", user);
+      await next();
+    } else {
+      return c.json(
+        {
+          message: "You Must Login First",
+        },
+        403
+      );
+    }
   }
 }
 
