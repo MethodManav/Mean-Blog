03cb331263196a66bd22e83ca4c8a61dfed057a2
Behera Manav <manavbehera123@gmail.com>
2025-01-06 21:55:57 +0530

    added cores

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,10 +4,11 @@ import { blogRouter } from "./routes/blog.route";
 import { subjectRouter } from "./routes/subject.route";
 import { chapterRouter } from "./routes/chapters.route";
 import { notesRouter } from "./routes/note.route";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 const path = "api/v1/";
-
+app.use("/api/*", cors());
 app.route(`${path}user`, userRouter);
 app.route(`${path}blog`, blogRouter);
 app.route(`${path}subject`, subjectRouter);
